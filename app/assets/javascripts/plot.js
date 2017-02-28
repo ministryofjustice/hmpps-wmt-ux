45e228a859048a8fab4db9cbf5c8768ae124c293
chrisisk <me@chrisisk.com>
2017-02-28 13:58:10 +0000

    adding new js for graph

--- app/assets/javascripts/plot.js
+++ app/assets/javascripts/plot.js
@@ -1,7 +1,80 @@
-var data = [{
-  x: ['giraffes', 'orangutans', 'monkeys'],
-  y: [20, 14, 23],
-  type: 'bar'
-}];
-
-Plotly.newPlot('myDiv', data);
+trace1 = {
+  x: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'], 
+  y: ['88', '90', '100', '118', '117', '111', '107', '118', '80', '99', '102', '105'], 
+  hoverinfo: 'x+y', 
+  line: {width: 3.5}, 
+  marker: {
+    line: {width: -0.5}, 
+    size: 9
+  }, 
+  mode: 'lines+markers', 
+  name: 'Utilisation %', 
+  type: 'scatter', 
+  uid: '1f15fb', 
+  xsrc: 'chrisisk:3:2e4c95', 
+  ysrc: 'chrisisk:3:87e338'
+};
+data = [trace1];
+layout = {
+  autosize: true, 
+  dragmode: 'pan', 
+  hovermode: 'closest', 
+  legend: {
+    x: 0.436873550022, 
+    y: -0.208008047869, 
+    borderwidth: 0, 
+    font: {size: 12}, 
+    orientation: 'h'
+  }, 
+  margin: {
+    r: 0, 
+    t: 40, 
+    b: 60, 
+    l: 50
+  }, 
+  shapes: [
+    {
+      fillcolor: 'rgb(109, 206, 250)', 
+      line: {
+        color: 'rgba(68, 68, 68, 100)', 
+        width: 0
+      }, 
+      opacity: 0.3, 
+      type: 'rectangle', 
+      x0: 0, 
+      x1: 1, 
+      xref: 'paper', 
+      y0: 90, 
+      y1: 110, 
+      yref: 'y'
+    }
+  ], 
+  showlegend: true, 
+  title: 'Caseload Utilisation', 
+  xaxis: {
+    autorange: true, 
+    mirror: false, 
+    nticks: 0, 
+    range: [-0.689098746526, 11.6890987465], 
+    showgrid: false, 
+    showline: true, 
+    side: 'bottom', 
+    ticks: 'outside', 
+    title: '2016', 
+    titlefont: {size: 15}, 
+    type: 'category', 
+    zeroline: false, 
+    zerolinewidth: 11
+  }, 
+  yaxis: {
+    autorange: false, 
+    range: [0, 120], 
+    ticks: 'inside', 
+    title: 'Utilisation %', 
+    type: 'linear'
+  }
+};
+Plotly.plot('plotly-div', {
+  data: data,
+  layout: layout
+});
